a79e5842243e3c8176b4ef5421d06e2b51e403f7
Alvinjasper <alvinjspr@gmail.com>
2023-01-06 13:53:41 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    result = 0
-    for argument in range(1, len(argv)):
-        result = result + int(argv[argument])
-     print("{}".format(result))
+    import sys
+    argv = sys.argv[1:]
+    argv_count = len(argv)
+    index = 1
+    res = 0
+    while index <= argv_count:
+        res += int(sys.argv[index])
+        index += 1
+        print("{:d}".format(res))
